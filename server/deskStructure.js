8d65eb0f81374f846aa7beed8b831e61ee94320d
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2021-12-06 15:54:08 -0500

    No desk structure

--- server/deskStructure.js
+++ server/deskStructure.js
@@ -8,18 +8,6 @@ export default () => {
     S.list()
         .title('Content')
         .items([
-            S.documentListItem()
-                .schemaType('siteSettings')
-                .title('Site Settings')
-                .child(
-                    S.document()
-                        .schemaType('siteSettings')
-                        .documentId('siteSettings')
-                ),
-            S.divider(),
-            pages,
-            team,
-            pageBuilder,
-            ...S.documentTypeListItems().filter(item => !['siteSettings'].includes(item.getId()))
+            ...S.documentTypeListItems()
         ])
 }
