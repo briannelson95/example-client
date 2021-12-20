fd3f7910f1f96fee25b31a38e477678a6b3d58d4
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2021-12-19 19:56:11 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -31,6 +31,38 @@ export default function Navbar() {
                                     )}
                                 </Disclosure.Button>
                             </div>
+                            <div className='flex-1 flex items-center justify-centet sm:items-stretch sm:justify-start'>
+                                <div className='flex-shrink-0 flex items-center'>
+                                    <img 
+                                        className='block lg:hidden h-8 w-auto'
+                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                                        alt="Workflow"
+                                    />
+                                    <img
+                                        className="hidden lg:block h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                                        alt="Workflow"
+                                    />
+                                </div>
+                                <div className='hidden sm:block sm:ml-6'>
+                                    <div className='flex space-x-4'>
+                                        {navigation.map((item) => (
+                                            <a 
+                                                key={item.name}
+                                                href={item.href}
+                                                className={classNames(
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 
+                                                    'px-3 py-2 rounded-md text-sm font-medium'
+                                                )}
+                                                aria-current={item.current ? 'page' : undefined}
+                                            >
+                                                {item.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </>
